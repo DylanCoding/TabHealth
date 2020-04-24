6158cee83cd75997dc51248dfb1fef1a9b41034f
DylanCoding <dylangregory@outlook.com>
2020-04-24 16:25:47 +0100

    Added absorption

--- src/me/baneofsmite/tabhealth/Main.java
+++ src/me/baneofsmite/tabhealth/Main.java
@@ -2,6 +2,7 @@ package me.baneofsmite.tabhealth;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scoreboard.*;
@@ -35,7 +36,7 @@ public class Main extends JavaPlugin { //Leon Is Awesome, Play on Arctic. Also B
 
         Bukkit.getScheduler().runTaskTimer(this, () -> {
             for (Player player : Bukkit.getOnlinePlayers()) {
-                int health = (int) Math.floor((player.getHealth() / 20) * 100);
+                int health = (int) Math.floor(((player.getHealth() / 20) * 100) + ((CraftPlayer) player).getHandle().getAbsorptionHearts());
                 
                 name.getScore(player.getName()).setScore(health);
                 tab.getScore(player.getName()).setScore(health);
